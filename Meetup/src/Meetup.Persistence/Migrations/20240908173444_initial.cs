63b948d796a36c28e7f94e629a1efeae0f01c40f
MarkSmentina <mark.smentina@bk.ru>
2024-09-08 20:43:30 +0300

    add other

---
+++ Meetup/src/Meetup.Persistence/Migrations/20240908173444_initial.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Meetup.Persistence.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    HouseNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    RoomNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeetingType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    MeetingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetingType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meeting",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MeetingTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meeting", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meeting_MeetingType_MeetingTypeId",
+                        column: x => x.MeetingTypeId,
+                        principalTable: "MeetingType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocationMeeting",
+                columns: table => new
+                {
+                    LocationsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MeetingsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationMeeting", x => new { x.LocationsId, x.MeetingsId });
+                    table.ForeignKey(
+                        name: "FK_LocationMeeting_Location_LocationsId",
+                        column: x => x.LocationsId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LocationMeeting_Meeting_MeetingsId",
+                        column: x => x.MeetingsId,
+                        principalTable: "Meeting",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeetingTag",
+                columns: table => new
+                {
+                    MeetingsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetingTag", x => new { x.MeetingsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_MeetingTag_Meeting_MeetingsId",
+                        column: x => x.MeetingsId,
+                        principalTable: "Meeting",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MeetingTag_Tag_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tag",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationMeeting_MeetingsId",
+                table: "LocationMeeting",
+                column: "MeetingsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meeting_MeetingTypeId",
+                table: "Meeting",
+                column: "MeetingTypeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeetingTag_TagsId",
+                table: "MeetingTag",
+                column: "TagsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocationMeeting");
+
+            migrationBuilder.DropTable(
+                name: "MeetingTag");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Meeting");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "MeetingType");
+        }
+    }
+}
