29b690144ea0f046b8c8b2f818538cfbb05740e4
Сментин Марк Денисович <M.Smentina@cbt.by>
2024-09-09 14:43:09 +0300

    add Sentry to the project

---
+++ src/Meetup.API/Configurations/Sentry/SentryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Meetup.API.Configurations.Sentry
+{
+    public static class SentryConfiguration
+    {
+        public static IHostBuilder ConfigureWebHostWithSentry(this IHostBuilder builder)
+        {
+            return builder.ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseSentry(o =>
+                {
+                    o.Dsn = "https://adb0b8abbb2f783bfc848475ade6c155@o4507921863671808.ingest.de.sentry.io/4507921867341904";
+                    o.Debug = true;
+                    o.TracesSampleRate = 1.0;
+                });
+            });
+        }
+    }
+}
