447837cc2fab04d6d0e3d56a7ded6d6276fd6cf1
MarkSmentina <mark.smentina@bk.ru>
2024-10-17 23:06:44 +0300

    .

--- src/Meetup.Persistence/Repositories/TagRepository.cs
+++ src/Meetup.Persistence/Repositories/TagRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace Meetup.Persistence.Repositories
+﻿using Meetup.Domain.Interfaces.Repositories;
+using Meetup.Domain.Models;
+using Meetup.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meetup.Persistence.Repositories
 {
-    public class TagRepository
+    public class TagRepository : ITagRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public TagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            return await _context.Tags.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<bool> CreateAsync(Tag tag)
+        {
+            _ = await _context.Tags.AddAsync(tag);
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            Tag item = await _context.Tags.FindAsync(id);
+
+            if (item != null)
+            {
+                _ = _context.Remove(item);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<Tag> GetByIdAsync(Guid id)
+        {
+            return await _context.Tags.FindAsync(id);
+        }
+
+        public Task<bool> UpdateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
