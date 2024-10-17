0e50308ec59b4375f762cca0ae368d39c9374820
MarkSmentina <mark.smentina@bk.ru>
2024-10-17 23:07:27 +0300

    base tag service

--- src/Meetup.Application/Services/TagService.cs
+++ src/Meetup.Application/Services/TagService.cs
@@ -1,6 +1,40 @@
-﻿namespace Meetup.Application.Services
+﻿using Meetup.Application.DTOs;
+using Meetup.Application.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meetup.Application.Services
 {
-    public class TagService
+    public class TagService : IBaseService<TagDTO>, ITagService
     {
+        public TagService()
+        {
+            
+        }
+
+        public Task<TagDTO> Create(TagDTO item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TagDTO> Get(TagDTO item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<TagDTO>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TagDTO> Remove(TagDTO item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TagDTO> Update(TagDTO Item)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
