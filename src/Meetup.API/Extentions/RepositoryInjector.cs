cd0f437103174ba6e1ed304b96a4ec80b693c492
MarkSmentina <mark.smentina@bk.ru>
2024-10-22 21:25:55 +0300

    add base extentions

---
+++ src/Meetup.API/Extentions/RepositoryInjector.cs
@@ -0,0 +1,17 @@
+﻿using Meetup.Domain.Interfaces.Repositories;
+using Meetup.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meetup.API.Extentions
+{
+    public static class RepositoryInjector
+    {
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<IMeetingRepository, MeetingRepository>();
+            services.AddScoped<IMeetingTypeRepository, MeetingTypeRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+        }
+    }
+}
---
+++ src/Meetup.API/Extentions/ServiceInjector.cs
@@ -0,0 +1,17 @@
+﻿using Meetup.Application.Interfaces;
+using Meetup.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meetup.API.Extentions
+{
+    public static class ServiceInjector
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITagService, TagService>();
+            services.AddScoped<IMeetingService, MeetingService>();
+            services.AddScoped<ILocationService, LocationService>();
+            services.AddScoped<IMeetingTypeService, MeetingTypeService>();
+        }
+    }
+}
